9d465fee430754ed9a4c00aa0b6be250e0e37e74
bytebard8 <peeksinthepast@gmail.com>
2024-03-29 00:47:15 -0400

    modified:   src/App.jsx 	modified:   src/main.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,6 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
 import './App.css'
 import Container from '@mui/material/Container';
 import DrawerAppBar from './components/NavigationBar.jsx';
@@ -6,6 +9,7 @@ import Home from './views/Home.jsx';
 import Footer from './components/Footer.jsx'
 
 function App() {
+  const [count, setCount] = useState(0)
 
   return (
     <Container fixed disableGutters>
