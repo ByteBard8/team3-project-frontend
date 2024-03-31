3abbcc4fd8db654fb3e56c9ed763a6e4a46a5fe7
Ian Villanueva <ian.villanueva@thesalesmachine.com>
2024-03-31 15:40:28 -0400

    first and last name fields in signup

--- src/views/SignUp.jsx
+++ src/views/SignUp.jsx
@@ -1,76 +1,96 @@
 import React, { useState } from 'react';
-import { TextField, Container, Typography,Stack } from '@mui/material';
-import Button from "../components/Button.jsx"
+import { TextField, Container, Typography, Stack } from '@mui/material';
+import Button from "../components/Button.jsx";
 
+export default function SignUp() {
+    const [formData, setFormData] = useState({
+        email: '',
+        firstName: '',
+        lastName: '',
+        password: '',
+        confirmPassword: ''
+    });
 
-export default function SignIn() {
-
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-  
-    const handleSignUp = () => {
-      console.log('Email:', email);
-      console.log('Password:', password);
+    const handleChange = (e) => {
+        setFormData({ ...formData, [e.target.name]: e.target.value });
     };
+
+    const handleSignUp = () => {
+        // Create a JSON object from the form data
+        const userData = {
+            email: formData.email,
+            firstName: formData.firstName,
+            lastName: formData.lastName,
+            password: formData.password,
+            confirmPassword: formData.confirmPassword
+        };
+
+        // Now you can send `userData` to your backend API
+        console.log('User Data:', userData);
+    };
+
     return (
-        <Container component="main" maxwidth="xs">
-        <div>
-        <Typography variant="h5" sx={{ mb: 2 }} align="left">
-        Sign Up
-    </Typography>
-    <Stack spacing={1}>
-          <TextField
-            variant="outlined"
-            label="Email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            required
-            color="primary"
-          />
-            <TextField
-            variant="outlined"
-            label="Last name"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            required
-            color="primary"
-          />
-            <TextField
-            variant="outlined"
-            label="First name"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            required
-            color="primary"
-          />
-          <TextField
-            variant="outlined"
-            label="Password"
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-            color="primary"
-          />
-            <TextField
-            variant="outlined"
-            label="Confirm password"
-            type="password"
-            value={password}
-            onChange={(e) => setEmail(e.target.value)}
-            required
-            color="primary"
-          />
-          <Button
-            color="primary"
-            onClick={handleSignUp}
-            buttontext="Sign up"
-            minwidth="60px"
-            maxwidth="60px"
-            />
-            <Button variant="text" href="/signin" buttontext="Go back to Sign in" />
-        </Stack>
-        </div>
-      </Container>
+        <Container component="main" maxWidth="xs">
+            <div>
+                <Typography variant="h5" sx={{ mb: 2 }} align="left">
+                    Sign Up
+                </Typography>
+                <Stack spacing={1}>
+                    <TextField
+                        variant="outlined"
+                        label="Email"
+                        name="email"
+                        value={formData.email}
+                        onChange={handleChange}
+                        required
+                        color="primary"
+                    />
+                    <TextField
+                        variant="outlined"
+                        label="First Name"
+                        name="firstName"
+                        value={formData.firstName}
+                        onChange={handleChange}
+                        required
+                        color="primary"
+                    />
+                    <TextField
+                        variant="outlined"
+                        label="Last Name"
+                        name="lastName"
+                        value={formData.lastName}
+                        onChange={handleChange}
+                        required
+                        color="primary"
+                    />
+                    <TextField
+                        variant="outlined"
+                        label="Password"
+                        type="password"
+                        name="password"
+                        value={formData.password}
+                        onChange={handleChange}
+                        required
+                        color="primary"
+                    />
+                    <TextField
+                        variant="outlined"
+                        label="Confirm Password"
+                        type="password"
+                        name="confirmPassword"
+                        value={formData.confirmPassword}
+                        onChange={handleChange}
+                        required
+                        color="primary"
+                    />
+                    <Button
+                        color="primary"
+                        onClick={handleSignUp}
+                        buttontext="Sign up"
+                    />
+                    <Button variant="text" href="/signin" buttonText="Go back to Sign in" />
+                </Stack>
+            </div>
+        </Container>
     );
 }
